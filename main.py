2076677f2fdbc0de12ebd1467a82347817d37033
Doug Gordon <gordononline@bigpond.com>
2023-12-15 19:46:33 +1100

    letter complete

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#
+PLACEHOLDER = "[name]"
+
+
+with open("input/Letters/starting_letter.txt", "r") as file:
+    letter = file.read()
+
+with open("input/Names/invited_names.txt", "r") as file:
+    invites = file.readlines()
+    for name in invites:
+        stripped_name = name.strip()
+        new_letter = letter.replace(PLACEHOLDER, stripped_name)
+        with open(
+            f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w"
+        ) as completed_letter:
+            completed_letter.write(new_letter)
